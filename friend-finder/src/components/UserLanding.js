ba96fff9fec19726248c9c75b03050f24410b51d
Joseph Luna <josephluna@Josephs-MBP.local>
2019-07-14 17:10:29 -0500

    adding react-bootstrap and updating landing page code

--- friend-finder/src/components/UserLanding.js
+++ friend-finder/src/components/UserLanding.js
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Button from "react-bootstrap/Button";
 
 class UserLanding extends Component {
+
+        state = {
+            isLoggedIn: false
+        }
+
+
     render() {
         return (
-            
-                <div className="landing-wrapper">
-                    <div className="navigation-wrapper"> 
-                        <h1>Friends Finder</h1>  // this needs to be a component for navigation in the app
-                        <img className="logo" src="" />
-                        <ul>
-                            <Link to="/"><li>Home</li></Link>
-                            <Link to="/login"><li>Login</li></Link>
-                        </ul>
-                    </div>
+            <>
+            <div className="navigation-wrapper"> 
+                        <div className="logo-wrapper">
+                        <h1>Friends Finder</h1>  
+                        <img className="logo" src="https://www.pngkey.com/png/full/59-592141_friend-silhouette-at-getdrawings-friend-png.png" alt="friend-logo" />
+                        </div>
 
-                    <div className="hero-img">
+                        <ul className="link-wrapper">
+                           <li><Link to="/">Home</Link></li> |
+                           {
+                               this.state.isLoggedIn? 
+                            <li><Link to="/">LogOut</Link></li>:
+                            <li><Link to="/login">Login</Link></li> 
+                            }
+                            
+                        </ul>
+            </div>
+
+                <div className="landing-wrapper">
+
+
+                    <div className="hero-img"></div>
+                       <div className="landing-wrapper-content">
                         <h1>Friends Helping Find Friends</h1>
                         <h2>Join our community today and find new friends!</h2>
-                        <p>come on friend were ready to meet you.</p>
-                        <Link to="/signup"><button>Sign Up!</button></Link>
-                    </div>
+                        <h3>come on friend we're ready to meet you.</h3>
+                        <Link to="/signup"><Button variant="primary" size="lg">Sign Up!</Button></Link>
+                       </div>
+                    
                     
                 </div>
-
+</>
         )
     }
 }
-//
 
 export default UserLanding;
