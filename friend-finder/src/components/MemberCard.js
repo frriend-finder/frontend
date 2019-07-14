0f74c18bb60565c4ca258e8b2d83a468d1c39642
schang1146 <sammychang2185@gmail.com>
2019-07-14 15:57:22 -0700

    MemberCard scaffolded

--- friend-finder/src/components/MemberCard.js
+++ friend-finder/src/components/MemberCard.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class MemberCard extends Component {
     render() {
         return (
-            <></>
+            <div className='member-card'>
+                {/* Info on other members to show */}
+                <p>Picture:</p>
+                <p>Name:</p>
+                <p>Age:</p>
+                <p>City:</p>
+                <button>Match</button>
+                <button>Move On</button>
+            </div>
         )
     }
 }
