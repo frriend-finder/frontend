5e447ef2b34d7e04926aafe1acc4113e13e6f374
schang1146 <sammychang2185@gmail.com>
2019-07-14 19:48:54 -0700

    added PrivateRoute to redirect if not logged in

--- friend-finder/src/components/PrivateRoute.js
+++ friend-finder/src/components/PrivateRoute.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
-class PrivateRoute extends Component {
-    render() {
-        return (
-            <></>
-        )
-    }
+const PrivateRoute = ({ component: Component, ...theRest }) => {
+    return (
+        <Route
+            {...theRest}
+            render = {() => {
+                if (localStorage.getItem('token')) {
+                    return <Component />;
+                } else {
+                    return <Redirect to='/login' />;
+                }
+            }}
+        />
+    )
 }
 
 export default PrivateRoute;
