410f8709c3a288d66e55be596ad3ec9d8f1bb18d
schang1146 <sammychang2185@gmail.com>
2019-07-18 19:47:16 -0700

    sign up and login works

--- friend-finder/src/actions/index.js
+++ friend-finder/src/actions/index.js
@@ -5,19 +5,31 @@ export const ADD_NEW_USER_START = "ADD_NEW_USER_START";
 export const ADD_NEW_USER_SUCCESS = "ADD_NEW_USER_SUCCESS";
 export const ADD_NEW_USER_FAILURE = "ADD_NEW_USER_FAILURE";
 export const ADD_TO_FRIENDS = "ADD_TO_FRIENDS";
+export const SEND_CODE_START = "SEND_CODE_START";
+export const SEND_CODE_SUCCESS = "SEND_CODE_SUCCESS";
+export const SEND_CODE_FAILURE = "SEND_CODE_FAILURE";
 
-export const login = creds => dispatch => {
+export const login = (email, code) => dispatch => {
     dispatch({ type: LOGIN_START });
     return axios
-        .post()
-        .then(res => localStorage.setItem('token', 'faketoken123'))
+        .post('https://friendfinder-bw19.herokuapp.com/auth/verify', { email, code })
+        .then(res => console.log(res))
+        .then(res => localStorage.setItem('token', res.data.payload))
+        .catch(err => console.log(err));
+}
+
+export const sendCode = email => dispatch => {
+    dispatch({ type: SEND_CODE_START });
+    return axios
+        .post('https://friendfinder-bw19.herokuapp.com/auth/send', { email })
+        .then()
         .catch(err => console.log(err));
 }
 
 export const addNewUser = (user) => dispatch => {
     dispatch({ type: ADD_NEW_USER_START })
     return axios
-        .post('https://friendfinder-bw19.herokuapp.com/user/', user)
+        .post('https://friendfinder-bw19.herokuapp.com/user', user)
         .then(res => dispatch({ type: ADD_NEW_USER_SUCCESS, payload: res.data }))
         .catch(err => dispatch({ type: ADD_NEW_USER_FAILURE, payload: err.data }));
 }
--- friend-finder/src/components/Navigation.js
+++ friend-finder/src/components/Navigation.js
@@ -8,27 +8,27 @@ class Navigation extends React.Component{
         isLoggedIn: true  //need to make a change here and a conditional in the className to toggle the class name depending if this is true or not
     }
 
-render(){
-    return(
+    render() {
+        return(
 
-    <div className="navigation-wrapper"> 
-        <div className="logo-wrapper">
-            <h1>Friends Finder</h1>  
-             <img className="logo" src="https://www.pngkey.com/png/full/59-592141_friend-silhouette-at-getdrawings-friend-png.png" alt="friend-logo" />
+        <div className="navigation-wrapper"> 
+            <div className="logo-wrapper">
+                <h1>Friends Finder</h1>  
+                    <img className="logo" src="https://www.pngkey.com/png/full/59-592141_friend-silhouette-at-getdrawings-friend-png.png" alt="friend-logo" />
+            </div>
+
+            <ul className="link-wrapper">
+
+                {
+                    this.state.isLoggedIn? 
+                    <><li><Link to="/">Home</Link></li> | <li><Link to="/">Log Out</Link></li> | <li><Link to="/memberslist">Find Friends</Link></li> </> :
+                    <><li><Link to="/">Home</Link></li> | <li><Link to="/login">Login</Link></li> </>
+                }
+                
+            </ul>
         </div>
-
-        <ul className="link-wrapper">
-          
-           {
-            this.state.isLoggedIn? 
-            <><li><Link to="/">Home</Link></li> | <li><Link to="/">Log Out</Link></li> | <li><Link to="/memberslist">Find Friends</Link></li> </> :
-            <><li><Link to="/">Home</Link></li> | <li><Link to="/login">Login</Link></li> </>
-            }
-            
-        </ul>
-     </div>
-    )
-   }
+        )
+    }
 
 }
 
